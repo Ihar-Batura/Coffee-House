d8da7cc01f8fc66576c73a893e85eb6f7d6d04e8
Ihar-Batura <A17331582@gmail.com>
2024-09-23 16:48:23 +0300

    fix: fix slider js

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -2,90 +2,101 @@ const sliderLine = document.querySelector('.layout-1-column');
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const sliderIndicators = document.querySelectorAll('.indicator');
-const sliderCard = document.querySelector('.favCoffee__card').offsetWidth;
+const sliderCard = document.querySelector('.favCoffee__card');
+const sliderCards = document.querySelectorAll('.favCoffee__card');
+let windowWidth;
+
 let position = 0;
 let sliderIndex = 0;
 
+// Check width window and slider card
+function initWindowWidth() {
+  windowWidth = window.innerWidth;
+  if (windowWidth < 650) {
+    touchMoveSlider(); // включается мониторинг прикосновений к дисплею
+  }
+}
+window.addEventListener('resize', initWindowWidth); //check change window width
+initWindowWidth();
 
-//Functions
+//Functions move slider
 
 const nextSlide = () => {
-    if (position < 696 && sliderCard === 348) {
-        position += 348;
-        sliderIndex++;
+  if (windowWidth >= 515) {
+    if (position < 960) {
+      position += 480;
+      sliderIndex++;
+    } else {
+      position = 0;
+      sliderIndex = 0;
     }
-    else if(position < 960 && sliderCard === 480) {
-        position += 480;
-        sliderIndex++;  
+  } else {
+    if (position < 696) {
+      position += 348;
+      sliderIndex++;
+    } else {
+      position = 0;
+      sliderIndex = 0;
     }
-    else {
-        position = 0;
-        sliderIndex = 0;
-    }
-    sliderLine.style.left = -position + "px";
-    thisSlide(sliderIndex);
+  }
+  sliderLine.style.left = -position + 'px';
+  thisSlide(sliderIndex);
+};
 
-    
-}
 const prevSlide = () => {
-    if(position > 0 && sliderCard === 348) {
-        position -= 348;
-        sliderIndex--;
+  if (windowWidth >= 515) {
+    if (position > 0) {
+      position -= 480;
+      sliderIndex--;
+    } else {
+      position = 960;
+      sliderIndex = 2;
     }
-    else if(position > 0 && sliderCard === 480) {
-        position -= 480;
-        sliderIndex--;  
+  } else {
+    if (position > 0) {
+      position -= 348;
+      sliderIndex--;
+    } else {
+      position = 696;
+      sliderIndex = 2;
     }
-    else {
-        position = 960; //Здесь могут быть вопросы при мобильном экране!!! Позиция 696
-        sliderIndex = 2;
-    }
-    sliderLine.style.left = -position + "px";
-    thisSlide(sliderIndex);
-}
+  }
+  sliderLine.style.left = -position + 'px';
+  thisSlide(sliderIndex);
+};
 
 //Слайдер Индекс меняет цвет
 const thisSlide = (index) => {
-    for (let indicator of sliderIndicators) {
-        indicator.classList.remove("indicator__progress");
-    }
-    sliderIndicators[index].classList.add("indicator__progress");
-}
-
+  for (let indicator of sliderIndicators) {
+    indicator.classList.remove('indicator__progress');
+  }
+  sliderIndicators[index].classList.add('indicator__progress');
+};
 
 //EventListeners
 
 arrowRight.addEventListener('click', nextSlide);
 arrowLeft.addEventListener('click', prevSlide);
 
-
 //SetInterval
 
-setInterval( ()=> {
-    nextSlide()
-   
-},7000)
+setInterval(() => {
+  nextSlide();
+}, 7000);
 
+function touchMoveSlider() {
+  let x1 = null;
+  let y1 = null;
 
-//Touch работает но криво, нужно доделать!!!
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-
-const slider = document.querySelector('.favCoffee__card');
-
-let x1 = null;
-let y1 = null;
-
-function handleTouchStart(event) {
+  function handleTouchStart(event) {
     const firstTouch = event.touches[0];
     x1 = firstTouch.clientX;
     y1 = firstTouch.clientY;
-    console.log(x1, y1);
-}
+  }
 
-function handleTouchMove(event) {
+  function handleTouchMove(event) {
     if (!x1 || !y1) {
-        return false;
+      return false;
     }
     let x2 = event.touches[0].clientX;
     let y2 = event.touches[0].clientY;
@@ -94,18 +105,20 @@ function handleTouchMove(event) {
     let yDiff = y2 - y1;
 
     if (Math.abs(xDiff) > Math.abs(yDiff)) {
-        if (xDiff > 0) {
-            nextSlide();
-        }
-        else {
-            prevSlide();
-        }
+      if (xDiff > 0) {
+        nextSlide();
+      } else {
+        prevSlide();
+      }
     }
     x1 = null;
     y1 = null;
+  }
+
+  sliderCards.forEach((card) => {
+    card.addEventListener('touchstart', handleTouchStart);
+    card.addEventListener('touchmove', handleTouchMove);
+  });
 }
 
-
-
-
-
+touchMoveSlider();
