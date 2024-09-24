b2484d2481de1a3ad586563ad7af56ed6b39ea96
Ihar-Batura <A17331582@gmail.com>
2024-09-24 16:44:01 +0300

    fix: fix code for open modal window

--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -1,23 +1,33 @@
 const cardsList = document.querySelectorAll('.coffee__card');
 const buttonsList = document.querySelectorAll('.button_transparent');
 const downloadBtn = document.querySelector('.button__refresh');
+let isTeaOrDessert;
 
 // Загрузка дополнительных карточек
 function downloadCards() {
-  menuCard5.classList.add('active');
-  menuCard6.classList.add('active');
-  menuCard7.classList.add('active');
-  menuCard8.classList.add('active');
+  cardsList.forEach((card) => card.classList.add('active'));
   downloadBtn.classList.add('active');
 }
 
 downloadBtn.addEventListener('click', downloadCards);
 
 function changeCategory(category) {
-  console.log(category);
   if (category !== 'Tea' && category !== 'Dessert') {
     deleteClass();
+    cardsList.forEach((card) => card.classList.remove('active'));
+    downloadBtn.classList.remove('active'); // включает кнопку дополнительной загрузки
+    isTeaOrDessert = null;
   } else {
+    // отключает кнопку дополнительной загрузки в категории чай и включает в остальных
+    if (category === 'Tea') {
+      downloadBtn.classList.add('active');
+      cardsList.forEach((card) => card.classList.remove('active'));
+      isTeaOrDessert = 'tea';
+    } else {
+      downloadBtn.classList.remove('active'); // включает кнопку дополнительной загрузки
+      cardsList.forEach((card) => card.classList.remove('active'));
+      isTeaOrDessert = 'dessert';
+    }
     deleteClass();
     addClass(category);
   }
@@ -83,5 +93,110 @@ function changeCardText(data) {
       }
     }
   }
-  const cardText = cardsList[0].childNodes[3].children.length;
+}
+
+/* MODAL WINDOW */
+
+//Открытие модального окна
+const modal = document.querySelector('.modal');
+const body = document.querySelector('.body');
+
+function openModal() {
+  modal.classList.add('active');
+  body.classList.add('active');
+}
+
+function closeModal() {
+  modal.classList.remove('active');
+  body.classList.remove('active');
+}
+
+cardsList.forEach((card) => {
+  card.addEventListener('click', () => {
+    const classImg = card.childNodes[1].classList;
+    openModal();
+    createModal();
+    addImgToModal(classImg);
+  });
+});
+
+function addImgToModal(classImg) {
+  const modalImg = document.querySelector('.modal__img');
+  if (isTeaOrDessert) {
+    modalImg.classList.add(isTeaOrDessert);
+  }
+  modalImg.classList.add(classImg[1]);
+}
+
+function createModal() {
+  const modalWrapper = document.querySelector('.modal__window');
+  const modalWindow = document.createElement('div');
+  modalWindow.className = 'modal__window';
+  modalWindow.innerHTML = `
+        <div class="modal__img"></div>
+          <div class="window__info-box">
+            <h3 class="h3">Irish coffee</h3>
+            <p class="modal__text modal__text-main">
+              Fragrant black coffee with Jameson Irish whiskey and whipped milk
+            </p>
+            <p class="modal__text modal__text-choice">Size</p>
+            <div class="window__button-box">
+              <button class="modal__button active">
+                <p class="modal__button-icon active">S</p>
+                200 ml
+              </button>
+              <button class="modal__button">
+                <p class="modal__button-icon ">M</p>
+                300 ml
+              </button>
+              <button class="modal__button">
+                <p class="modal__button-icon ">L</p>
+                400 ml
+              </button>
+            </div>
+            <p class="modal__text modal__text-choice text-choice__2">
+              Additives
+            </p>
+            <div class="window__button-box">
+              <button class="modal__button">
+                <p class="modal__button-icon ">1</p>
+                Sugar
+              </button>
+              <button class="modal__button">
+                <p class="modal__button-icon ">2</p>
+                Cinnamon
+              </button>
+              <button class="modal__button">
+                <p class="modal__button-icon ">3</p>
+                Syrup
+              </button>
+            </div>
+            <div class="total__box">
+              <h3 class="h3">Total:</h3>
+              <h3 class="h3">$7.00</h3>
+            </div>
+            <div class="info-box">
+              <img
+                class="info-box__logo"
+                src="../assets/icons/info-empty.svg"
+                alt="logo info"
+              />
+              <p class="info-box__text">
+                The cost is not final. Download our mobile app to see the final
+                price and place your order. Earn loyalty points and enjoy your
+                favorite coffee with up to 20% discount.
+              </p>
+            </div>
+            <button class="modal__button modal__button-main btn-close">Close</button>
+          </div>
+`;
+  modalWrapper.appendChild(modalWindow);
+
+  //Закрытие модального окна через кнопку!
+  const btnClose = document.querySelector('.btn-close');
+
+  btnClose.addEventListener('click', () => {
+    closeModal();
+    modalWrapper.innerHTML = '';
+  });
 }
